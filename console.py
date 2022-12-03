53b0032228572cd2eeea8454f0ad3a5b354d34a2
EndrisMoh <endrismd@gmail.com>
2022-12-03 12:21:47 -0800

    Pycodestyle console error fixed

--- console.py
+++ console.py
@@ -4,14 +4,15 @@
 
 import cmd
 from datetime import datetime
-import models
-from models.amenity import Amenity
+import models 
+from models import storage
 from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
 import shlex  # for splitting the line along spaces except in double quotes
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
